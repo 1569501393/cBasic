940fb7ab7c0e065f823bcee5089eaa2f8033599c
揭衍强 <jieyanqiang@lf-network.com>
2024-01-31 09:25:35 +0800

    feat: 10-loops

--- 10-loops/main.c
+++ 10-loops/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    for (;;) {
-        printf("loop forever\n");
-    }
+    // for (;;) {
+    //     printf("loop forever\n");
+    // }
 
+    for (int i = 0; i < 10; ++i) {
+        printf("i=%d\n", i);
+    }
     return 0;
 }
