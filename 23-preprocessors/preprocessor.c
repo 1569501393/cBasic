def3f9095031735e8fc1d9fe030d69351924d134
揭衍强 <jieyanqiang@lf-network.com>
2024-02-05 17:39:22 +0800

    feat: 23-preprocessors

--- 23-preprocessors/preprocessor.c
+++ 23-preprocessors/preprocessor.c
@@ -2,6 +2,13 @@
 
 #define MAX_ARRAY_LENGTH 20
 
+// #define message_for(a, b) \
+//     printf(#a " and " #b ": We love you!\n")
+
+#define message_for(a, b) printf(#a " and " #b ": We love you!\n")
+
+#define MAX(x, y) (x > y ? x: y)
+
 int main() {
     printf("MAX_ARRAY_LENGTH = %d \n", MAX_ARRAY_LENGTH);
     printf("__DATE__ = %s \n", __DATE__);
@@ -11,5 +18,8 @@ int main() {
     printf("__LINE__ = %d \n", __LINE__);
     printf("__STDC__ = %d \n", __STDC__);
 
-	return 0;
+    message_for(Carole, Debra);
+    printf("Maxbetween 10 and 20 is %d \n", MAX(10, 20));
+
+    return 0;
 }
